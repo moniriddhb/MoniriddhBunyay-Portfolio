4d7676676d92201df78d93455b471cf8101bb9c9
moniriddhb <moniriddhbunyay@gmail.com>
2026-04-18 20:39:59 -0700

    Lab 3 changes including updated contact form

--- global.js
+++ global.js
@@ -1,5 +1,88 @@
-console.log('IT’S ALIVE!');
+console.log("IT'S ALIVE!");
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+const pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact" },
+  { url: "resume/", title: "Resume" },
+  { url: "https://github.com/moniriddhb", title: "GitHub Profile" }
+];
+
+const BASE_PATH =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/"
+    : "/MoniriddhBunyay-Portfolio/"; // replace website with your GitHub repo name
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  url = !url.startsWith("http") ? BASE_PATH + url : url;
+
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = title;
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add("current");
+  }
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+
+  nav.append(a);
+}
+
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+`
+);
+
+let select = document.querySelector(".color-scheme select");
+
+let saved = localStorage.colorScheme;
+
+if (saved) {
+  document.documentElement.style.setProperty("color-scheme", saved);
+  select.value = saved;
+}
+
+select.addEventListener("input", function (event) {
+  let value = event.target.value;
+
+  document.documentElement.style.setProperty("color-scheme", value);
+  localStorage.colorScheme = value;
+});
+
+
+let form = document.querySelector("form");
+
+form?.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let data = new FormData(form);
+  let url = form.action + "?";
+
+  for (let [name, value] of data) {
+    url += `${name}=${encodeURIComponent(value)}&`;
+  }
+
+  location.href = url;
+});
